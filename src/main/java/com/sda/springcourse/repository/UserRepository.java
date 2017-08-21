d559bd6d0af48f5f728b8d1231f803666f0ea628
PolandPoznanUla <ula_dam@o2.pl>
2017-08-21 20:28:40 +0200

    form for adding user

--- src/main/java/com/sda/springcourse/repository/UserRepository.java
+++ src/main/java/com/sda/springcourse/repository/UserRepository.java
@@ -5,9 +5,12 @@ import com.sda.springcourse.model.User;
 import java.util.List;
 
 public interface UserRepository {
+
     List<User> getAll();
 
     List<User> getAllByLastName(String lastName);
 
     User getById(Integer id);
+
+    boolean add(User user);
 }
